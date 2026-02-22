e660060417a34f550939c34321be8e1c4fbd878f
Liam Gimbel <ljgimbel@captechu.edu>
2026-02-21 21:01:46 -0500

    remove all debug stuff

--- target/debug/boggle_solver.d
+++
@@ -1,1 +0,0 @@
-C:\Users\Gimbe\Programming\rust\boggle_solver\target\debug\boggle_solver.exe: C:\Users\Gimbe\Programming\rust\boggle_solver\src\main.rs
--- target/debug/deps/boggle_solver.d
+++
@@ -1,5 +0,0 @@
-C:\Users\Gimbe\Programming\rust\boggle_solver\target\debug\deps\boggle_solver.d: src\main.rs
-
-C:\Users\Gimbe\Programming\rust\boggle_solver\target\debug\deps\boggle_solver.exe: src\main.rs
-
-src\main.rs:
